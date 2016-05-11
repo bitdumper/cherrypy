18d12d1083fbb043072d57b79d5a0ee7bdd56884
Shivendra Kumar <bitdumper.irc@gmail.com>
2016-05-11 17:50:30 +0530

    typo

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ r = redis.StrictRedis(host='localhost',port= 6379 , db = 0 )
 def index():
  r.set('current',request.forms.get('i'))
  return 'ok'
-run(hist='0.0.0.0',port=15403,debug=True)
+run(host='0.0.0.0',port=15403,debug=True)
