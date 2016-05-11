9778ca48cead8ca96ea1217fbaa156abdfad1a20
Shivendra Kumar <bitdumper.irc@gmail.com>
2016-05-11 17:58:46 +0530

    method

--- server.py
+++ server.py
@@ -4,5 +4,9 @@ r = redis.StrictRedis(host='localhost',port= 6379 , db = 0 )
 @route('/')
 def index():
  r.set('current',request.forms.get('i'))
- return 'ok'
+ return request.forms.get('i')
+@route('/t')
+def infdex():
+ r.set('current',request.forms.get('i'))
+ return request.forms.get('i')
 run(host='0.0.0.0',port=15403,debug=True)
