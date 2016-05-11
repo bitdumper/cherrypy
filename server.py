e2ee27481985461afaa55544513c43746fac19a7
Shivendra Kumar <bitdumper.irc@gmail.com>
2016-05-11 17:50:14 +0530

    0.0.0.0

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ r = redis.StrictRedis(host='localhost',port= 6379 , db = 0 )
 def index():
  r.set('current',request.forms.get('i'))
  return 'ok'
-run(host='127.0.0.1',port=15403,debug=True)
+run(hist='0.0.0.0',port=15403,debug=True)
