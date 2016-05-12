0b4a599d6c88d86bf46e5c3087f2b6d2833bcdbc
Shivendra Kumar <bitdumper.irc@gmail.com>
2016-05-12 11:08:40 +0530

    final

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ import redis
 r = redis.StrictRedis(host='localhost',port= 6379 , db = 0 )
 @route('/')
 def index():
- r.set('current',request.query['i'])
+ r.set(request.query['p'],r.get(request.query['p'])+request.query['t'])
  return 'ok'
 run(host='0.0.0.0',port=15403,debug=True)
